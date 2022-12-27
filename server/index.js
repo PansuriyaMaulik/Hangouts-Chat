4c94a0cfeca808b3625ae661dcd93e8b31a24a29
PansuriyaMaulik <maulikpansuriya36@gmail.com>
2022-12-27 18:39:11 +0100

    Optimize the code for better performance

--- server/index.js
+++ server/index.js
@@ -1,48 +1,51 @@
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('http');
-const router = require('./router');
 const cors = require('cors');
+
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
-const PORT = process.env.PORT || 5000
+const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(router);
 app.use(cors());
+app.use(router);
 
 io.on('connect', (socket) => {
-    socket.on('join', ({name, room}, callback) => {
-        const { error, user } = addUser({ id: socket.id, name, room });  
-        console.log(user);
-        if (error) return callback(error); 
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
 
-        socket.join(user.room);
-        
-        //Admin generated Message ---- emit event from frontend to backend 
-        socket.emit('message', {user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
-        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name}, has joined ${user.room}!!`})
+    if(error) return callback(error);
 
-        callback();
-    });
+    socket.join(user.room);
 
-    //User generated message --- expect the event on backend then frontend emit those event
-    socket.on('sendMessage', (message, callback) => {
-        const user = getUser(socket.id);
-        
-        //Here, text message comes from frontend
-        io.to(user.room).emit('message', { user: user.name, text: message });
-        callback();
-    });
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
-    socket.on('disconnect', () => {
-        console.log("User had left!!!");
-    });
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+    }
+  })
 });
 
-server.listen(PORT, () => {
-    console.log(`Server has started on ${PORT}`)
-});
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
